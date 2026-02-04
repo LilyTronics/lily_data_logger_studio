06d5ba2b02715d0fde2cad40bc8d228787c19480
Danny <danny.v.der.pol@gmail.com>
2026-02-04 20:15:22 +0100

    using app from app data

--- src/main.py
+++ src/main.py
@@ -2,17 +2,14 @@
 Run the data logger.
 """
 
-import wx
-
 import src.app_data as AppData
 
 from src.controllers.controller_main import MainController
 
 
 def run_data_logger():
-    app = wx.App(False)
     MainController(f"{AppData.APP_NAME} V{AppData.VERSION}")
-    app.MainLoop()
+    AppData.wxApp.MainLoop()
 
 
 if __name__ == "__main__":
