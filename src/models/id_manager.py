cb27869f4a83c8bdca2340e128d126c8e750b8a9
Danny <danny.v.der.pol@gmail.com>
2026-02-21 14:29:27 +0100

    add IDs for the menu

--- src/models/id_manager.py
+++ src/models/id_manager.py
@@ -4,6 +4,11 @@ ID manager for the controls
 
 import wx
 
+
+# Menu
+ID_MENU_NEW_CONFIG = wx.NewIdRef()
+ID_MENU_EXIT = wx.NewIdRef()
+
 # Toolbar icons
 ID_SHOW_DATA_TABLE = wx.NewIdRef()
 ID_SHOW_LOG = wx.NewIdRef()
