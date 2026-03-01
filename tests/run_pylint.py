a237d34de338192a980bf6a01befb57e51f50b4a
Danny <danny.v.der.pol@gmail.com>
2026-03-01 16:04:59 +0100

    add script for running pylint on all sources

---
+++ tests/run_pylint.py
@@ -0,0 +1,44 @@
+"""
+Run pylint
+
+Use this as a guideline to check if code is more or less consistent.
+It is not used to solve every issue, because pylint is also not perfect.
+"""
+
+import os
+import pylint
+
+arguments = [
+    # The wx library is not properly handled
+    "--ignored-modules=wx",
+    # Cyclic imports are not properly handled
+    "--disable=cyclic-import",
+    # We do not want doc strings everywhere, we believe in self-explanatory code
+    "--disable=missing-class-docstring",
+    "--disable=missing-function-docstring",
+    # We determine ourselves how we handle exceptions, thank you very much
+    "--disable=broad-exception-caught",
+    "--disable=broad-exception-raised",
+    # Not relevant how many public methods there are
+    "--disable=too-few-public-methods",
+    # We decide how many is too many, thank you very much
+    "--disable=too-many-instance-attributes"
+    # # We have our own preferred import order
+    # "--disable=wrong-import-order"
+]
+
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+for current_folder, sub_folders, filenames in os.walk(root_path):
+    sub_folders.sort()
+    # Skip files in build output
+    if current_folder.startswith(os.path.join(root_path, "build", "build_output")):
+        continue
+    if current_folder.startswith(os.path.join(root_path, "temp")):
+        continue
+    for filename in filter(lambda x: x.endswith(".py"), filenames):
+        filepath = os.path.join(current_folder, filename)
+        print(f"Adding: {filepath}")
+        arguments.append(filepath)
+
+
+pylint.run_pylint(arguments)
