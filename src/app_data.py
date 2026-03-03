989aca532d9eb8f21c5c0ac18ec5b1385d7e0593
Danny <danny.v.der.pol@gmail.com>
2026-03-03 19:36:24 +0100

    change the order of things

--- src/app_data.py
+++ src/app_data.py
@@ -14,12 +14,6 @@ VERSION = "0.1"
 EXE_NAME = "LilyDataLoggerStudio"
 COMPANY = "LilyTronics"
 
-wxApp.SetAppName(EXE_NAME)
-_sp = wx.StandardPaths.Get()
-
-SETTINGS_FILE = os.path.join(_sp.GetUserDataDir(), f"{EXE_NAME}.json")
-APP_LOG_FILE = os.path.join(_sp.GetUserDataDir(), f"{EXE_NAME}.log")
-
 # Application path depends on if run from script or from the executable
 if EXE_NAME in sys.executable:
     APP_PATH = os.path.dirname(sys.executable)
@@ -28,11 +22,17 @@ else:
 
 INSTRUMENTS_PATH = os.path.join(APP_PATH, "instruments")
 
+wxApp.SetAppName(EXE_NAME)
+_sp = wx.StandardPaths.Get()
+
+SETTINGS_FILE = os.path.join(_sp.GetUserDataDir(), f"{EXE_NAME}.json")
+APP_LOG_FILE = os.path.join(_sp.GetUserDataDir(), f"{EXE_NAME}.log")
+
 
 if __name__ == "__main__":
 
     print(f"{APP_NAME} V{VERSION}")
-    print("Settings file:", SETTINGS_FILE)
-    print("App log file:", APP_LOG_FILE)
     print("App path:", APP_PATH)
     print("Instruments path:", INSTRUMENTS_PATH)
+    print("Settings file:", SETTINGS_FILE)
+    print("App log file:", APP_LOG_FILE)
