a24cdb338a46e8906cecadf68d8d093fb193d489
Danny <danny.v.der.pol@gmail.com>
2026-03-06 21:41:46 +0100

    add progress dialog

---
+++ src/views/view_progress_dialog.py
@@ -0,0 +1,61 @@
+"""
+Progress dialog.
+"""
+
+import wx
+
+
+class ProgressDialog(wx.ProgressDialog):
+
+    _TIMER_INTERVAL = 100
+
+    def __init__(self, parent, title, maximum):
+        super().__init__(title, " ", maximum, parent, wx.PD_CAN_ABORT | wx.PD_APP_MODAL)
+        self.Fit()
+        self.CenterOnParent()
+        self._timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self._on_timer, self._timer)
+        self._timer.Start(self._TIMER_INTERVAL)
+
+    ##################
+    # Event handlers #
+    ##################
+
+    def _on_timer(self, event):
+        if not self.Update(self.GetValue())[0]:
+            self.destroy()
+        event.Skip()
+
+    ##########
+    # Public #
+    ##########
+
+    def destroy(self):
+        self._timer.Stop()
+        self.Destroy()
+
+    def update(self, value, message=wx.EmptyString):
+        do_continue = True
+        if value < self.GetRange():
+            do_continue = self.Update(value, message)[0]
+            self.Fit()
+            self.CenterOnParent()
+        else:
+            self.destroy()
+        return do_continue
+
+
+if __name__ == "__main__":
+
+    import time
+
+    app = wx.App(redirect=False)
+
+    dlg = ProgressDialog(None, "Test progress", 100)
+
+    for i in range(1, 101):
+        if not dlg.update(i, f"item {i}"):
+            break
+        time.sleep(0.2)
+
+    dlg.destroy()
