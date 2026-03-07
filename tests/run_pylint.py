8b4f277381b7c0f6374ee2d9c48b20c2df09868e
Danny <danny.v.der.pol@gmail.com>
2026-03-07 21:05:16 +0100

    moved options from script to config file

--- tests/run_pylint.py
+++ tests/run_pylint.py
@@ -3,30 +3,14 @@ Run pylint
 
 Use this as a guideline to check if code is more or less consistent.
 It is not used to solve every issue, because pylint is also not perfect.
+See .pylintrc for the options
 """
 
 import os
 import pylint
 
-arguments = [
-    # The wx library is not properly handled
-    "--ignored-modules=wx",
-    # Cyclic imports are not properly handled
-    "--disable=cyclic-import",
-    # We do not want doc strings everywhere, we believe in self-explanatory code
-    "--disable=missing-class-docstring",
-    "--disable=missing-function-docstring",
-    # We determine ourselves how we handle exceptions, thank you very much
-    "--disable=broad-exception-caught",
-    "--disable=broad-exception-raised",
-    # Not relevant how many public methods there are
-    "--disable=too-few-public-methods",
-    # We decide how many is too many, thank you very much
-    "--disable=too-many-instance-attributes",
-    # Duplicate code is not always duplicate code...
-    "--disable=duplicate-code"
-]
 
+arguments = []
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 for current_folder, sub_folders, filenames in os.walk(root_path):
     sub_folders.sort()
@@ -43,6 +27,5 @@ for current_folder, sub_folders, filenames in os.walk(root_path):
         filepath = os.path.join(current_folder, filename)
         print(f"Adding: {filepath}")
         arguments.append(filepath)
-
 
 pylint.run_pylint(arguments)
