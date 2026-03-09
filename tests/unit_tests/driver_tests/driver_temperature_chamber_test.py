67a53c0cfe8770010b931e4e7b6fc10fac0d60ec
Danny <danny.v.der.pol@gmail.com>
2026-03-09 20:58:41 +0100

    add test for the temperature chamber simulator driver

---
+++ tests/unit_tests/driver_tests/driver_temperature_chamber_test.py
@@ -0,0 +1,33 @@
+"""
+Test the temperature chamber driver for the simulator.
+"""
+
+from src.models.drivers import Drivers
+from src.simulators.run_simulators import start_simulators
+from src.simulators.run_simulators import stop_simulators
+from src.simulators.simulator_settings import SimulatorSettings
+from tests.lib.test_suite import TestSuite
+
+
+class DriverTemperatureChamberTest(TestSuite):
+
+    driver = None
+
+    def setup(self):
+        self.log.debug("Start simulators")
+        start_simulators()
+        Drivers.load()
+        driver_class = Drivers.get_driver("TemperatureChamber")
+        self.fail_if(driver_class is None, "The driver is not found")
+        self.driver = driver_class(SimulatorSettings.TemperatureChamber)
+
+    def teardown(self):
+        self.log.debug("Stop simulators")
+        stop_simulators()
+
+    def test_driver(self):
+        pass
+
+if __name__ == "__main__":
+
+    DriverTemperatureChamberTest().run()
