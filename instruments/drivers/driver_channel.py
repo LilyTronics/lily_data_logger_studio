84d2062e5bc5671205ee52d136dfed7851b41f62
Danny <danny.v.der.pol@gmail.com>
2026-03-10 19:54:28 +0100

    replaced asserts with raise exception

--- instruments/drivers/driver_channel.py
+++ instruments/drivers/driver_channel.py
@@ -11,15 +11,24 @@ class DriverChannel:
     DIR_OUTPUT = "output"
 
     def __init__(self, channel_id, name, expect_response=True):
-        assert isinstance(channel_id, str), "The channel ID must be a string"
-        assert channel_id != "", "The channel ID cannot be empty"
-        assert re.fullmatch(r"[a-zA-Z0-9_-]+", channel_id) is not None, \
-            "The channel ID must be alphanumeric and can contain underscores and hyphens"
-        assert isinstance(name, str), "The channel name must be a string"
-        assert name != "", "The channel name cannot be empty"
-        assert name.startswith("get ") or name.startswith("set "), \
-            "The channel name must start with 'get' or 'set'"
-        assert isinstance(expect_response, bool), "The expect_response must be a boolean"
+        if not isinstance(channel_id, str):
+            raise TypeError("Channel ID must be a string")
+        if not channel_id:
+            raise ValueError("Channel ID cannot be empty")
+        if re.fullmatch(r"[a-zA-Z0-9_-]+", channel_id)  is None:
+            raise ValueError(
+                "Channel ID must be alphanumeric and may contain underscores and hyphens"
+            )
+        if not isinstance(name, str):
+            raise TypeError("Channel name must be a string")
+        if not name:
+            raise ValueError("Channel name cannot be empty")
+        if not (name.startswith("get ") or name.startswith("set ")):
+            raise ValueError(
+                "Channel name must start with 'get ' or 'set '"
+            )
+        if not isinstance(expect_response, bool):
+            raise TypeError("Expect response must be a boolean")
 
         self.channel_id = channel_id
         self.name = name
--- instruments/drivers/driver_settings.py
+++ instruments/drivers/driver_settings.py
@@ -10,12 +10,18 @@ class DriverSetting:
     _VALID_CONTROLS = [CTRL_TEXT]
 
     def __init__(self, name, setting_type, default_value, gui_control):
-        assert setting_type in self._VALID_TYPES, \
-            f"The type {setting_type} for {name} is not supported"
-        assert isinstance(default_value, setting_type), \
-            f"The default value for {name} has the wrong type"
-        assert gui_control in self._VALID_CONTROLS, \
-            f"The control {gui_control} for {name} is not supported"
+        if setting_type not in self._VALID_TYPES:
+            raise ValueError(
+                f"Setting type {setting_type} for '{name}' is not supported"
+            )
+        if not isinstance(default_value, setting_type):
+            raise TypeError(
+                f"Default value for '{name}' must be of type {setting_type.__name__}"
+            )
+        if gui_control not in self._VALID_CONTROLS:
+            raise ValueError(
+                f"GUI control '{gui_control}' for '{name}' is not supported"
+            )
 
         self.name = name
         self.type = setting_type
