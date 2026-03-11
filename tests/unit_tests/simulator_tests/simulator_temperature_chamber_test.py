9e91ee01d3c008a68bbee10e04640c719ff0c6a7
Danny <danny.v.der.pol@gmail.com>
2026-03-11 21:30:06 +0100

    updated test for simulator to latest updates

--- tests/unit_tests/simulator_tests/simulator_temperature_chamber_test.py
+++ tests/unit_tests/simulator_tests/simulator_temperature_chamber_test.py
@@ -11,11 +11,24 @@ from tests.lib.udp_client import UdpClient
 
 class SimulatorTemperatureChamberTest(TestSuite):
 
+    _TIMEOUT = 15
+
     def setup(self):
         start_simulators()
 
     def teardown(self):
         stop_simulators()
+
+    def _wait_for_temperature(self, tc, target_temp):
+        t = self._TIMEOUT
+        while t > 0:
+            temp = tc.send_command("temp?")
+            self.log.debug(f"Current temperature: {temp}")
+            if target_temp - 0.5 < float(temp) < target_temp + 0.5:
+                return True
+            self.sleep(3)
+            t -= 3
+        return False
 
     def test_temperature_chamber(self):
         self.log.debug("Connect to temperature chamber")
@@ -28,27 +41,21 @@ class SimulatorTemperatureChamberTest(TestSuite):
         self.log.debug(f"Initial temperature: {temp}")
         self.fail_if(temp != 20.0, f"Unexpected initial temperature: {temp}")
         self.log.debug("Set temperature to 30")
-        response = tc.send_command('temp=30')
+        response = tc.send_command("temp=30")
         self.log.debug(f"Response: {response}")
         self.fail_if(response != "ok", "Invalid response received. Expected 'ok'.")
-        result = self.wait_for(lambda: 29.5 < float(tc.send_command("temp?")) < 30.5, True,
-                               timeout=10, interval=1)
-        self.log.debug(f"Current temperature: {tc.send_command('temp?')}")
+        result = self._wait_for_temperature(tc, 30.0)
         self.fail_if(result, "Temperature should not have reached 30 degrees")
         self.log.debug("Switch chamber on")
-        response = tc.send_command('on')
-        self.log.debug(f"Response: {response}")
+        tc.send_command("pwr=1", False)
         self.fail_if(response != "ok", "Invalid response received. Expected 'ok'.")
-        result = self.wait_for(lambda: 29.5 < float(tc.send_command("temp?")) < 30.5, True,
-                               timeout=10, interval=1)
-        self.log.debug(f"Current temperature: {tc.send_command('temp?')}")
-        self.fail_if(not result, "Temperature did not have reach 30 degrees")
+        result = self._wait_for_temperature(tc, 30.0)
+        self.fail_if(not result, "Temperature did not reach 30 degrees")
         self.log.debug("Switch chamber off")
-        response = tc.send_command('off')
-        self.log.debug(f"Response: {response}")
-        temp = float(tc.send_command("temp?"))
+        tc.send_command("pwr=0", False)
+        result = self._wait_for_temperature(tc, 20.0)
         self.log.debug(f"Current temperature: {temp}")
-        self.fail_if(temp != 20.0, f"Unexpected temperature: {temp}")
+        self.fail_if(not result, f"Temperature did not return to 20 degrees")
 
 
 if __name__ == "__main__":
