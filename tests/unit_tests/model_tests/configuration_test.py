d87377315b2819746dc3535c5184d2fb9bcbf26e
Danny <danny.v.der.pol@gmail.com>
2026-03-17 19:42:19 +0100

    make test independent of the order of the group names

--- tests/unit_tests/model_tests/configuration_test.py
+++ tests/unit_tests/model_tests/configuration_test.py
@@ -9,7 +9,7 @@ from tests.lib.test_suite import TestSuite
 
 class ConfigurationTest(TestSuite):
 
-    _EXPECTED_GROUPS = ["settings", "instruments", "process", "measurements", "graphs"]
+    _EXPECTED_GROUPS = ["settings", "instruments", "measurements", "process", "graphs"]
     _configuration = Configuration()
 
     def setup(self):
@@ -20,7 +20,7 @@ class ConfigurationTest(TestSuite):
     def test_default_config(self):
         groups = self._configuration.get_main_groups()
         self.log.debug(f"Main groups: {groups}")
-        self.fail_if(list(groups) != self._EXPECTED_GROUPS,
+        self.fail_if(sorted(list(groups)) != sorted(self._EXPECTED_GROUPS),
                      f"Invalid main groups. Expected: {self._EXPECTED_GROUPS}")
         for group in groups:
             sub_items = self._configuration.get_sub_items(group)
