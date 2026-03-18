79ff41dbddaafe5cf57997f512c7642fe4798489
Danny <danny.v.der.pol@gmail.com>
2026-03-18 07:16:52 +0100

    add class for simulating application logger

--- tests/lib/test_suite.py
+++ tests/lib/test_suite.py
@@ -7,8 +7,17 @@ import lily_unit_test
 
 class TestSuite(lily_unit_test.TestSuite):
 
+    # Works as the application logger for objects needing a logger
+    class _TestLogger():
+        def __init__(self, logger):
+            self._logger = logger
+
+        def debug(self, message):
+            self._logger.debug(f"(test logger) {message}")
+
     def __init__(self, *args):
         super().__init__(*args)
+        self.test_logger = self._TestLogger(self.log)
 
 
 if __name__ == "__main__":
