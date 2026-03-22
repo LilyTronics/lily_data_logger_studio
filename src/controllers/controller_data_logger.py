8d297693dc7b3e7098ff8495f38c8ec0b1111f0c
Danny <danny.v.der.pol@gmail.com>
2026-03-22 19:48:52 +0100

    removed data logger model (replaced by runners)

--- src/controllers/controller_data_logger.py
+++ src/controllers/controller_data_logger.py
@@ -2,11 +2,6 @@
 Controller for the data logger.
 """
 
-import wx
-
-from src.models.data_logger import DataLogger
-from src.views.view_dialogs import ViewDialogs
-
 
 class ControllerDataLogger:
 
@@ -14,15 +9,6 @@ class ControllerDataLogger:
         self._parent_view = parent_view
         self._configuration = configuration
         self._logger = logger
-        self._data_logger = DataLogger(self._configuration, self._on_data_logger_update)
-
-    ###########
-    # Private #
-    ###########
-
-    def _on_data_logger_update(self, data):
-        if "status" in data:
-            wx.CallAfter(self._parent_view.update_status, data["status"])
 
     ##########
     # Public #
@@ -30,16 +16,9 @@ class ControllerDataLogger:
 
     def start(self):
         self._logger.info("Start data logger")
-        try:
-            self._data_logger.start()
-        except Exception as e:
-            self._logger.error(f"Error starting data logger: {e}")
-            ViewDialogs.show_message(self._parent_view, f"Error starting data logger: {e}",
-                                     "Start data logger", wx.ICON_EXCLAMATION)
 
     def stop(self):
         self._logger.info("Stop data logger")
-        self._data_logger.stop()
 
 
 if __name__ == "__main__":
