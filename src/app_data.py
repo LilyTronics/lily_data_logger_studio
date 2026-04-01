720b332d60a8cf4881a49533bff0a2a8c7f65b60
Danny <danny.v.der.pol@gmail.com>
2026-04-01 08:15:24 +0200

    add constants for driver test application

--- src/app_data.py
+++ src/app_data.py
@@ -12,6 +12,9 @@ APP_NAME = "Lily Data Logger Studio\u2122"   # \u2122 is the trademark symbol
 VERSION = "0.6"
 EXE_NAME = "LilyDataLoggerStudio"
 COMPANY = "LilyTronics"
+DRIVER_TEST_APP_NAME = "Lily Data Logger Studio\u2122 Driver Test"
+DRIVER_TEST_EXE_NAME = "LilyDataLoggerStudioDriverTest"
+
 
 # Application path depends on if run from script or from the executable
 if EXE_NAME in sys.executable:
@@ -25,6 +28,7 @@ INSTRUMENTS_PATH = os.path.join(APP_PATH, "instruments")
 DRIVERS_PATH = os.path.join(INSTRUMENTS_PATH, "drivers")
 SETTINGS_FILE = os.path.join(get_user_data_dir(), EXE_NAME, f"{EXE_NAME}.json")
 APP_LOG_FILE = os.path.join(get_user_data_dir(), EXE_NAME, f"{EXE_NAME}.log")
+DRIVER_TEST_LOG_FILE = os.path.join(get_user_data_dir(), EXE_NAME, f"{DRIVER_TEST_EXE_NAME}.log")
 TEST_CONFIG_PATH = os.path.join(APP_PATH, "tests", "configurations")
 TEST_CONFIGURATION = os.path.join(TEST_CONFIG_PATH, "manual_test.json")
 
@@ -32,10 +36,10 @@ TEST_CONFIGURATION = os.path.join(TEST_CONFIG_PATH, "manual_test.json")
 if __name__ == "__main__":
 
     print(f"{APP_NAME} V{VERSION}")
-    print("App path        :", APP_PATH)
-    print("Instruments path:", INSTRUMENTS_PATH)
-    print("Drivers path    :", DRIVERS_PATH)
-    print("Settings file   :", SETTINGS_FILE)
-    print("App log file    :", APP_LOG_FILE)
-    print("App log file    :", APP_LOG_FILE)
-    print("Test config     :", TEST_CONFIGURATION)
+    print("App path             :", APP_PATH)
+    print("Instruments path     :", INSTRUMENTS_PATH)
+    print("Drivers path         :", DRIVERS_PATH)
+    print("Settings file        :", SETTINGS_FILE)
+    print("App log file         :", APP_LOG_FILE)
+    print("Driver test log file :", DRIVER_TEST_LOG_FILE)
+    print("Test config          :", TEST_CONFIGURATION)
