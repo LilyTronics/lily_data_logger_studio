2e466b417d3513f1d42eb41aabb9db20644093b7
Danny <danny.v.der.pol@gmail.com>
2026-04-01 14:17:40 +0200

    handle default value none and no settings

--- src/views/view_common.py
+++ src/views/view_common.py
@@ -10,7 +10,7 @@ import src.views.gui_sizes as GuiSizes
 def create_settings_grid(settings, grid, parent, controls_dict):
     controls_dict.clear()
     grid.Clear(True)
-    if settings is None:
+    if settings is None or len(settings) == 0:
         grid.Add(wx.Panel(parent), (0, 0))
     else:
         try:
@@ -18,7 +18,7 @@ def create_settings_grid(settings, grid, parent, controls_dict):
                 lbl = wx.StaticText(parent, wx.ID_ANY, f"{setting.name}:")
                 ctrl_class = getattr(wx, setting.gui_control)
                 ctrl = ctrl_class(parent, wx.ID_ANY, size=GuiSizes.WIDTH_MEDIUM)
-                ctrl.SetValue(str(setting.default_value))
+                ctrl.SetValue("" if setting.default_value is None else str(setting.default_value))
                 grid.Add(lbl, (i, 0), wx.DefaultSpan, wx.ALIGN_CENTER_VERTICAL)
                 grid.Add(ctrl, (i, 1), wx.DefaultSpan, wx.ALIGN_CENTER_VERTICAL)
                 controls_dict[setting.name] = (ctrl, setting.type)
