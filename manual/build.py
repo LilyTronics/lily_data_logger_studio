8a0074f78a195adf9a4fc6d02016613ae3c57dac
Danny <danny.v.der.pol@gmail.com>
2026-04-07 22:09:29 +0200

    add build manual script

---
+++ manual/build.py
@@ -0,0 +1,41 @@
+"""
+Build the documents
+"""
+
+import os
+import shutil
+import subprocess
+
+
+def build_manual(doc_name):
+
+    manual_dir = os.path.abspath(os.path.dirname(__file__))
+    source_dir = os.path.join(manual_dir, doc_name)
+    build_dir = os.path.join(manual_dir, "build", doc_name)
+
+    print(f"Manuals: {manual_dir}")
+    print(f"Source : {source_dir}")
+    print(f"Output : {build_dir}")
+
+    try:
+        shutil.rmtree(build_dir)
+        subprocess.run(
+            [
+                "sphinx-build",
+                "-b", "html",
+                "-c", manual_dir,
+                source_dir,
+                build_dir
+            ],
+            check=True,
+            capture_output=True,
+            text=True
+        )
+        print("Documentation built successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error building documentation: {e.stderr}")
+
+if __name__ == "__main__":
+
+    build_manual("main")
+    build_manual("driver_test")
