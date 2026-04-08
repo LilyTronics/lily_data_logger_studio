e07c47c7e7748b30702a5cde180c4a6999418e95
Danny <danny.v.der.pol@gmail.com>
2026-04-08 20:35:21 +0200

    fixed typo

--- instruments/drivers/driver_id.py
+++ instruments/drivers/driver_id.py
@@ -4,7 +4,7 @@ This ID is used to idetify the driver.
 This way class names and driver names can change without affecting exsiting configurations.
 
 Run this script to generate a new driver ID.
-Then coopy the generated ID to the driver class.
+Then copy the generated ID to the driver class.
 """
 
 import uuid
