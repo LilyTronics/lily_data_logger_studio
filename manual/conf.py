66558bd1dbf8fe53b596b56bfcba7934a334d8a9
Danny <danny.v.der.pol@gmail.com>
2026-04-08 09:01:25 +0200

    add options fot HTM output

--- manual/conf.py
+++ manual/conf.py
@@ -21,12 +21,13 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_logo = ''
+html_favicon = ''
+html_copy_source = False
+html_show_sourcelink = False
+html_show_sphinx = False
