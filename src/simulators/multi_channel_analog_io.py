772dce427f5088b363266a1badbd8674c372c4a9
Danny <danny.v.der.pol@gmail.com>
2026-04-10 21:43:44 +0200

    simulator working

--- src/simulators/multi_channel_analog_io.py
+++ src/simulators/multi_channel_analog_io.py
@@ -3,6 +3,8 @@ Multi channel analog IO simulator using TCP.
 """
 
 import socket
+import threading
+import time
 
 from src.simulators.simulator_base import SimulatorBase
 from src.simulators.simulator_settings import SimulatorSettings
@@ -15,9 +17,40 @@ class MultiChannelAnalogIo(SimulatorBase):
     _RX_BUFFER_SIZE = 1500
     _TERMINATOR = b"\n"
 
+    # Analog conversion from output to input
+    _CONVERSIONS = [
+        {"gain": 2, "speed": 0.1},
+        {"gain": 0.5, "speed": 0.05},
+    ]
+
     def __init__(self):
         super().__init__()
+        self._outputs = [0, 0]
+        self._inputs = [0, 0]
         self._sock = None
+        self._lock = threading.RLock()
+        self._analog_thread = threading.Thread(target=self._process_analog, daemon=True)
+        self._analog_thread.start()
+
+    ###########
+    # Private #
+    ###########
+
+    def _process_analog(self):
+        while not self._stop_event.is_set():
+            for i, conversion in enumerate(self._CONVERSIONS):
+                with self._lock:
+                    target = self._outputs[i] * conversion["gain"]
+                    if self._inputs[i] < target:
+                        self._inputs[i] += conversion["speed"]
+                    elif self._inputs[i] > target:
+                        self._inputs[i] -= conversion["speed"]
+                    self._inputs[i] = round(self._inputs[i], 3)
+            time.sleep(0.1)
+
+    #############
+    # Overrides #
+    #############
 
     def init(self):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,7 +62,6 @@ class MultiChannelAnalogIo(SimulatorBase):
         self._conn = None
 
     def run_handler(self):
-
         if self._conn is None:
             try:
                 self._conn, _ = self._sock.accept()
@@ -52,7 +84,20 @@ class MultiChannelAnalogIo(SimulatorBase):
                     data = data.strip()
                     if data == b"id?":
                         response = self.NAME.encode("utf-8")
-
+                    elif data.startswith(b"so,"):
+                        parts = data.split(b",")
+                        if len(parts) == 3:
+                            channel = int(parts[1])
+                            value = float(parts[2])
+                            with self._lock:
+                                self._outputs[channel - 1] = value
+                            response = b"ok"
+                    elif data.startswith(b"gi,"):
+                        parts = data.split(b",")
+                        if len(parts) == 2:
+                            channel = int(parts[1])
+                            with self._lock:
+                                response = b"%f" % self._inputs[channel - 1]
                 response += self._TERMINATOR
                 self._conn.sendall(response)
             except BlockingIOError:
--- tests/unit_tests/simulator_tests/simulator_analog_io_test.py
+++ tests/unit_tests/simulator_tests/simulator_analog_io_test.py
@@ -11,19 +11,39 @@ from tests.lib.tcp_client import TcpClient
 
 class SimulatorAnalogIoTest(TestSuite):
 
+    client = None
+    channel = 0
+    expected_values = [4, 1]
+
     def setup(self):
         start_simulators()
 
     def teardown(self):
         stop_simulators()
 
+    def check_input(self):
+        self.fail_if(not (1 <= self.channel <= 2), f"Invalid channel {self.channel}")
+        response = float(self.client.send_command(b"gi,%d" % self.channel))
+        return response == self.expected_values[self.channel - 1]
+
     def test_get_id(self):
         self.log.debug("Connect to analog IO")
-        io = TcpClient(SimulatorSettings.AnalogIo["host"],
-                       SimulatorSettings.AnalogIo["port"])
-        sim_id = io.send_command(b"id?")
+        self.client = TcpClient(SimulatorSettings.AnalogIo["host"],
+                                SimulatorSettings.AnalogIo["port"])
+        sim_id = self.client.send_command(b"id?")
         self.log.debug(f"ID: {sim_id}")
         self.fail_if(sim_id != b"Multi channel analog IO", f"Unexpected simulator ID: {sim_id}")
+
+    def test_set_output(self):
+        for self.channel in range(1, 3):
+            self.log.debug(f"Set IO channel {self.channel}")
+            response = self.client.send_command(b"so,%d,2" % self.channel)
+            self.fail_if(response != b"ok", f"Invalid response: {response}")
+            self.log.debug("Wait for input to follow")
+            result = self.wait_for(self.check_input, True, 10, 0.5)
+            response = float(self.client.send_command(b"gi,%d" % self.channel))
+            self.log.debug(f"Input value: {response}")
+            self.fail_if(not result, "Input did not follow the output")
 
 
 if __name__ == "__main__":
