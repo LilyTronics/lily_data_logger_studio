81133be17de14872e4067d538e972f17024c4476
Danny <danny.v.der.pol@gmail.com>
2026-04-13 20:00:56 +0200

    immediately update GUI when new measurement value is available

--- src/models/measurements_runner.py
+++ src/models/measurements_runner.py
@@ -46,6 +46,7 @@ class MeasurementsRunner:
         # Response within the sample window
         TestRuns.store_measurement(self._run_id, params["request_time"],
                                     params["measurement_id"], value)
+        self._update_callback(TestRuns.get_test_run(self._run_id))
 
     def _run_measurements(self):
         time.sleep(0.1)
@@ -73,10 +74,6 @@ class MeasurementsRunner:
             elif 0 < end_time <= time.time() - start:
                 self._stop_event.set()
             time.sleep(0.01)
-        # Wait for measurements to finish
-        while time.time() - sample_start < settings["sample_time"]:
-            time.sleep(0.1)
-        self._update_callback(TestRuns.get_test_run(self._run_id))
         self._logger.debug("Measurments runner stopped")
 
     ##########
