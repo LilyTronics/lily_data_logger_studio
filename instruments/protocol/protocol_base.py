f59fd59803ef141b6397ef4ee5b7f4bb82efcf46
Danny <danny.v.der.pol@gmail.com>
2026-04-15 15:21:43 +0200

    documented the protocols

--- instruments/protocol/protocol_base.py
+++ instruments/protocol/protocol_base.py
@@ -11,6 +11,20 @@ from typing import final
 
 
 class ProtocolBase(ABC):
+    """
+    Base class for all protocol classes.
+
+    :param transport:           Instance of a transport class to use for communication.
+    :param protocol_settings:   Settings for the protocol.
+    :param debug:               Debug value from driver.
+
+    The settings is a dictionary that contains the driver settings controlled by the user
+    and the fixed protocol settings defined in the driver (see driver base class).
+
+    A queue processor thread is started to process commands added to the queue by the driver.
+    This allows for asynchronous processing of commands and responses.
+    This prevents the driver from being blocked while waiting for a response from the instrument.
+    """
 
     def __init__(self, transport, protocol_settings, debug=""):
         self.transport = transport
@@ -64,6 +78,18 @@ class ProtocolBase(ABC):
 
     @final
     def process_command(self, channel, command, callback, callback_params):
+        """
+        Process a command by either adding it to the queue (if a callback is provided) or
+        processing it immediately (if no callback is provided).
+
+        :param channel:         DriverChannel instance to use for communication.
+        :param command:         Command to process.
+        :param callback:        Callback function to call with the response (if any).
+        :param callback_params: Parameters to pass to the callback function.
+
+        The command is provided by the build_command method of the driver.
+        The callback parameters are directly passed from the driver.
+        """
         if callable(callback):
             self.log_debug("Add data to queue")
             self._queue.put({
@@ -78,6 +104,9 @@ class ProtocolBase(ABC):
 
     @final
     def close(self):
+        """
+        Close the protocol by stopping the queue processor thread.
+        """
         if self._thread.is_alive():
             self._stop_event.set()
             self._thread.join()
@@ -87,16 +116,34 @@ class ProtocolBase(ABC):
     #############
 
     @abstractmethod
-    def build_packet(self, data):
-        pass
+    def build_packet(self, data) -> bytes:
+        """
+        Build a packet to send to the instrument.
+
+        :param data: Data to insert into the packet.
+
+        Must be overridden by the protocol class.
+        """
 
     @abstractmethod
-    def parse_packet(self, data):
-        pass
+    def parse_packet(self, data) -> bytes:
+        """
+        Parse a packet received from the instrument.
+
+        :param data: Data received from the instrument.
+
+        Must be overridden by the protocol class.
+        """
 
     @abstractmethod
-    def validate_response(self, response):
-        pass
+    def validate_response(self, response) -> bool:
+        """
+        Validate a response received from the instrument.
+
+        :param response: Response received from the instrument.
+
+        Must be overridden by the protocol class.
+        """
 
 
 if __name__ == "__main__":
