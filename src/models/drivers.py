05815b541d8abb6ebd9be6ed7cf9151ba364c5e8
Danny <danny.v.der.pol@gmail.com>
2026-04-21 20:41:37 +0200

    don't stop loading drivers on error, but report errors after loading

--- src/models/drivers.py
+++ src/models/drivers.py
@@ -68,25 +68,34 @@ class Drivers:
             total = len(driver_files)
             progress_callback(-1, f"Load {total} drivers", total)
             i = 0
+            exceptions = []
             for i, filename in enumerate(driver_files):
                 rel_path = filename[len(AppData.INSTRUMENTS_PATH) + 1:]
                 progress_callback(i, f"Load driver from: {rel_path} ({i + 1}/{total})")
                 name = os.path.basename(filename).split(".")[0]
-                spec = spec_from_file_location(name, str(filename))
-                module = module_from_spec(spec)
-                spec.loader.exec_module(module)
-                for attribute_name in dir(module):
-                    attribute = getattr(module, attribute_name)
-                    if inspect.isclass(attribute):
-                        classes = [x.__name__ for x in inspect.getmro(attribute)]
-                        if "DriverBase" in classes:
-                            classes.remove("object")
-                            classes.remove("DriverBase")
-                            if "ABC" in classes:
-                                classes.remove("ABC")
-                            if len(classes) > 0:
-                                cls._add_driver(attribute)
+                try:
+                    spec = spec_from_file_location(name, str(filename))
+                    module = module_from_spec(spec)
+                    spec.loader.exec_module(module)
+                    for attribute_name in dir(module):
+                        attribute = getattr(module, attribute_name)
+                        if inspect.isclass(attribute):
+                            classes = [x.__name__ for x in inspect.getmro(attribute)]
+                            if "DriverBase" in classes:
+                                classes.remove("object")
+                                classes.remove("DriverBase")
+                                if "ABC" in classes:
+                                    classes.remove("ABC")
+                                if len(classes) > 0:
+                                    cls._add_driver(attribute)
+                except Exception as e:
+                    exceptions.append((rel_path, str(e)))
             progress_callback(i, f"Drivers loaded ({i + 1}/{total})")
+            if len(exceptions) > 0:
+                message = "One or more drivers are not loaded due to errors:"
+                for path, error in exceptions:
+                    message += f"\n{path}: {error}"
+                raise Exception(message)
 
     @classmethod
     def get_drivers(cls):
