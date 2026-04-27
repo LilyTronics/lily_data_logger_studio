314065d3b2399a69a8b17a65ecdb18c7e622d317
Danny <danny.v.der.pol@gmail.com>
2026-04-27 21:40:34 +0200

    support all three operation modes

--- src/controllers/controller_data_logger.py
+++ src/controllers/controller_data_logger.py
@@ -3,12 +3,14 @@ Controller for the data logger.
 """
 
 import threading
+import time
 import wx
 
 from src.models.instrument_pool import InstrumentPool
 from src.models.measurements_pool import MeasurementsPool
 from src.models.measurements_runner import MeasurementsRunner
 from src.models.process_runner import ProcessRunner
+from src.models.time_converter import TimeConverter
 from src.simulators.run_simulators import start_simulators
 from src.simulators.run_simulators import stop_simulators
 from src.views.view_dialog_check_instruments import ViewDialogCheckInstruments
@@ -26,10 +28,40 @@ class ControllerDataLogger:
         self._process_runner = ProcessRunner(self._configuration, self._logger,
                                              self._parent_view.update_process)
         self._check_result = False
+        self._data_logger_thread = None
+        self._stop_event = threading.Event()
 
     ###########
     # Private #
     ###########
+
+    def _run_data_logger(self):
+        self._logger.debug("Run data logger")
+        if len(self._configuration.get_measurements()) > 0:
+            self._measurements_runner.start()
+        if len(self._configuration.get_process_steps()) > 0:
+            self._process_runner.start()
+
+        settings = self._configuration.get_settings()
+        mode = settings["mode"]
+        end_time = settings["end_time"]
+        self._logger.debug(f"Run mode: {mode}")
+        if mode == "fixed time":
+            self._logger.debug(f"End time: {TimeConverter.create_duration_time_string(end_time)}")
+        start = time.time()
+        while not self._stop_event.is_set():
+            if mode == "fixed time" and time.time() - start > end_time:
+                self._logger.debug("End time reached")
+                break
+            if mode == "process end" and not self._process_runner.is_running():
+                self._logger.debug("Process ended")
+                break
+            time.sleep(0.1)
+
+        self._measurements_runner.stop()
+        self._process_runner.stop()
+        stop_simulators()
+        self._logger.debug("Data logger stopped")
 
     def _check_configuration(self):
         dlg_title = "Check configuration"
@@ -48,7 +80,13 @@ class ControllerDataLogger:
                 self._parent_view, "No measurments or steps in the configuration.", dlg_title
             )
             return False
-
+        settings = self._configuration.get_settings()
+        if settings["mode"] == "process end" and not has_steps:
+            ViewDialogs.show_message(
+                self._parent_view, "Mode is set to process end, but no steps in the configuration.",
+                dlg_title
+            )
+            return False
         return True
 
     def _setup_instruments_pool(self):
@@ -112,15 +150,12 @@ class ControllerDataLogger:
         if dlg:
             wx.CallAfter(dlg.Close)
 
-    def _process_update(self, *params):
-        wx.CallAfter(self._parent_view.update_process, *params)
-
     ##########
     # Public #
     ##########
 
     def start(self):
-        if self.is_running():
+        if self._data_logger_thread is not None and self._data_logger_thread.is_alive():
             return
         if not self._check_configuration():
             return
@@ -130,20 +165,22 @@ class ControllerDataLogger:
             return
         if not self._check_instruments():
             return
+        if (len(self._configuration.get_measurements()) == 0 and
+            len(self._configuration.get_process_steps()) == 0):
+            return
 
-        if len(self._configuration.get_measurements()) > 0:
-            self._measurements_runner.start()
-        if len(self._configuration.get_process_steps()) > 0:
-            self._process_runner.start()
+        self._stop_event.clear()
+        self._data_logger_thread = threading.Thread(name="DataLogger",
+                                                    target=self._run_data_logger,
+                                                    daemon=True)
+        self._data_logger_thread.start()
 
     def stop(self):
-        self._measurements_runner.stop()
-        self._process_runner.stop()
-        stop_simulators()
-
-    def is_running(self):
-        return self._measurements_runner.is_running() or self._process_runner.is_running()
-
+        if self._data_logger_thread is not None:
+            if self._data_logger_thread.is_alive():
+                self._stop_event.set()
+                self._data_logger_thread.join()
+            self._data_logger_thread = None
 
 if __name__ == "__main__":
 
