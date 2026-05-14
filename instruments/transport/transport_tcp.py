bb4c5148cbd5f1d44c3f411b1964f861fd0c135c
Danny <danny.v.der.pol@gmail.com>
2026-05-14 21:20:38 +0200

    updated manual (WIP)

--- instruments/transport/transport_tcp.py
+++ instruments/transport/transport_tcp.py
@@ -1,5 +1,5 @@
 """
-Transport over IP/TCP.
+Transport over TCP.
 """
 
 import socket
@@ -8,16 +8,26 @@ from instruments.transport.transport_base import TransportBase
 
 
 class TransportTcp(TransportBase):
+    """
+    Transport layer for TCP connections.
+    """
 
     socket = None
 
     _BUFFER_SIZE = 1500
 
     def get_id(self):
+        """
+        The ID is defined by the IP address and port number.
+        In case host names are used, they are converted to the IP address.
+        """
         ip = socket.gethostbyname(self.transport_settings.get("host", None))
         return f"tcp_{ip}_{self.transport_settings.get("port", None)}"
 
     def is_connection_ready(self):
+        """
+        Check if a connection is active.
+        """
         try:
             address = self.socket.getpeername()
             self.log_debug(f"Connected to: {address[0]}:{address[1]}")
@@ -26,6 +36,9 @@ class TransportTcp(TransportBase):
             return False
 
     def connect(self):
+        """
+        Connect to the host.
+        """
         host = self.transport_settings.get("host", "")
         port = self.transport_settings.get("port", None)
         self.log_debug(f"Connect to: {host}:{port}")
@@ -35,9 +48,15 @@ class TransportTcp(TransportBase):
         self.socket.settimeout(0.001)
 
     def send(self, data):
+        """
+        Send data.
+        """
         self.socket.sendall(data)
 
     def receive(self):
+        """
+        Check for data and return the data.
+        """
         data = b""
         try:
             data = self.socket.recv(self._BUFFER_SIZE)
@@ -46,6 +65,9 @@ class TransportTcp(TransportBase):
         return data
 
     def close(self):
+        """
+        Close the socket.
+        """
         if self.socket is not None:
             self.socket.close()
 
---
+++ manuals/driver_dev/classes/transport_tcp.rst
@@ -0,0 +1,7 @@
+Transport TCP
+-------------
+
+The TCP transport layer provides a connection for TCP connections.
+
+.. literalinclude:: ../../../instruments/transport/transport_tcp.py
+    :pyobject: TransportTcp
